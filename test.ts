a22feae932dab79cb0f61d2255731c61768d601e
Steve Jeon <steve.jeon@qbs.edu.hk>
2026-05-13 01:38:59 +0900

    Fix typescript compilation error in test.ts

--- test.ts
+++ test.ts
@@ -1,4 +1,4 @@
 // tests go here; this will not be compiled when this package is used as an extension.
-QBS.setupMusic(NoteKey.C, ScaleType.Major, NoteKey.G, 3, 20);
-QBS.playNote(0, SoundEffect.Flat);
+QBS.setupMusic(QBS.NoteKey.C, QBS.ScaleType.Major, QBS.NoteKey.G, 3, 20);
+QBS.playNote(0, QBS.SoundEffect.Flat);
 QBS.stopNote();
