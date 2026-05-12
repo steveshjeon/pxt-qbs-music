96aa66527500ee4f489d923489ceebad90f88279
Steve Jeon <steve.jeon@qbs.edu.hk>
2026-05-13 01:14:00 +0900

    Add simple test logic

--- test.ts
+++ test.ts
@@ -1,1 +1,4 @@
 // tests go here; this will not be compiled when this package is used as an extension.
+qbs.setupMusic(NoteKey.C, ScaleType.Major, NoteKey.G, 3, 20);
+qbs.playNote(0, SoundEffect.Flat);
+qbs.stopNote();
